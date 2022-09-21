281a3cbffd4d226a2ab4f32dc4bf91af7aedf518
Bhaumik Muliya <bhaumikmuliya1304+github@gmail.com>
2022-09-21 13:01:29 +0530

    products according to category

--- src/constants/productlist.js
+++ src/constants/productlist.js
@@ -1,100 +1,223 @@
-const Products = [
-  {
-    image: 'AQUASOOTHE PLUS.jpg',
-    text: 'AQUASOOTHE PLUS'
-  },
-  {
-    image: 'BEPOPREC.jpg',
-    text: 'BEPOPREC'
-  },
-  {
-    image: 'FLUOROPREC.jpg',
-    text: 'FLUOROPREC'
-  },
-  {
-    image: 'FLUOROPREC-T.jpg',
-    text: 'FLUOROPREC-T'
-  },
-  {
-    image: 'MOXIPREC-KT.jpg',
-    text: 'MOXIPREC-KT'
-  },
-  {
-    image: 'PARAPREC-1.jpg',
-    text: 'PARAPREC'
-  },
-  {
-    image: 'SCIBROM.jpg',
-    text: 'SCIBROM'
-  },
-  {
-    image: 'SCICLOP.jpg',
-    text: 'SCICLOP'
-  },
-  {
-    image: 'SCIDORZ-T.jpg',
-    text: 'SCIDORZ-T'
-  },
-  {
-    image: 'SCIFRESH GEL.jpg',
-    text: 'SCIFRESH GEL'
-  },
-  {
-    image: 'SCIFRESH.jpg',
-    text: 'SCIFRESH'
-  },
-  {
-    image: 'SCIGAT.jpg',
-    text: 'SCIGAT'
-  },
-  {
-    image: 'SCI-HY.jpg',
-    text: 'SCI-HY'
-  },
-  {
-    image: 'SCILUBE.jpg',
-    text: 'SCILUBE'
-  },
-  {
-    image: 'SCIMOX.jpg',
-    text: 'SCIMOX'
-  },
-  {
-    image: 'SCIMOX-BR.jpg',
-    text: 'SCIMOX-BR'
-  },
-  {
-    image: 'SCIMOX-DX.jpg',
-    text: 'SCIMOX-DX'
-  },
-  {
-    image: 'SCIMOX-LP.jpg',
-    text: 'SCIMOX-LP'
-  },
-  {
-    image: 'SCIMOX-TB.jpg',
-    text: 'SCIMOX-TB'
-  },
-  {
-    image: 'SCINATA.jpg',
-    text: 'SCINATA'
-  },
-  {
-    image: 'SCINEP.jpg',
-    text: 'SCINEP'
-  },
-  {
-    image: 'SCIPRED.jpg',
-    text: 'SCIPRED'
-  },
-  {
-    image: 'TOBAPREC-LP.jpg',
-    text: 'TOBAPREC-LP'
-  },
-  {
-    image: 'TROPISCI-P.jpg',
-    text: 'TROPISCI-P'
-  }
-]
+export const categories = [
+  'All',
+  'ANTI FUNGAL',
+  'ANTI BIOTIC WITH COMBINATION',
+  'STEROID',
+  'MISC. PREPARATIONS',
+  'OINTMENTS',
+  'TEAR SUBSTITUTE',
+  'ANTI GLAUCOMA',
+  'ANTIOXIDANT TABLETS, CAPSULES & SYRUP',
+  'ANTI INFLAMMATORY & ANTI ALLERGIC'
+];
+
+const Products = {
+  "ANTI FUNGAL": [
+    {
+      image: 'AQUASOOTHE PLUS.jpg',
+      text: 'AQUASOOTHE PLUS',
+      category: 2,
+      description: []
+    },
+    {
+      image: 'BEPOPREC.jpg',
+      text: 'BEPOPREC',
+      category: 2,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+  ],
+
+  "ANTI BIOTIC WITH COMBINATION": [
+    {
+      image: 'FLUOROPREC.jpg',
+      text: 'FLUOROPREC',
+      category: 2,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'FLUOROPREC-T.jpg',
+      text: 'FLUOROPREC-T',
+      category: 5,
+      description: ['Moxifloxacin 0.5%', 'Other chemical', 'Test']
+    },
+    {
+      image: 'MOXIPREC-KT.jpg',
+      text: 'MOXIPREC-KT',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'PARAPREC-1.jpg',
+      text: 'PARAPREC',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIBROM.jpg',
+      text: 'SCIBROM',
+      category: 0,
+      description: ['Moxifloxacin 0.5%']
+    }
+  ],
+
+  "STEROID": [
+    {
+      image: 'SCICLOP.jpg',
+      text: 'SCICLOP',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "MISC. PREPARATIONS": [
+    {
+      image: 'SCIDORZ-T.jpg',
+      text: 'SCIDORZ-T',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIFRESH GEL.jpg',
+      text: 'SCIFRESH GEL',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIFRESH.jpg',
+      text: 'SCIFRESH',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "OINTMENTS": [
+    {
+      image: 'SCIGAT.jpg',
+      text: 'SCIGAT',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCI-HY.jpg',
+      text: 'SCI-HY',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCILUBE.jpg',
+      text: 'SCILUBE',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX.jpg',
+      text: 'SCIMOX',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-BR.jpg',
+      text: 'SCIMOX-BR',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-DX.jpg',
+      text: 'SCIMOX-DX',
+      category: 1,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "TEAR SUBSTITUTE": [
+    {
+      image: 'SCINATA.jpg',
+      text: 'SCINATA',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCINEP.jpg',
+      text: 'SCINEP',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIPRED.jpg',
+      text: 'SCIPRED',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'TOBAPREC-LP.jpg',
+      text: 'TOBAPREC-LP',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'TROPISCI-P.jpg',
+      text: 'TROPISCI-P',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "ANTI GLAUCOMA": [
+    {
+      image: 'SCIMOX-LP.jpg',
+      text: 'SCIMOX-LP',
+      category: 1,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-TB.jpg',
+      text: 'SCIMOX-TB',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "ANTIOXIDANT TABLETS, CAPSULES & SYRUP": [
+    {
+      image: 'SCIMOX-LP.jpg',
+      text: 'SCIMOX-LP',
+      category: 1,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-TB.jpg',
+      text: 'SCIMOX-TB',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ],
+
+  "ANTI INFLAMMATORY & ANTI ALLERGIC": [
+    {
+      image: 'SCIMOX-LP.jpg',
+      text: 'SCIMOX-LP',
+      category: 1,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-TB.jpg',
+      text: 'SCIMOX-TB',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-LP.jpg',
+      text: 'SCIMOX-LP',
+      category: 1,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    },
+    {
+      image: 'SCIMOX-TB.jpg',
+      text: 'SCIMOX-TB',
+      category: 0,
+      description: ['Moxifloxacin 0.5%', 'Other chemical']
+    }
+  ]
+}
 
 export default Products;
